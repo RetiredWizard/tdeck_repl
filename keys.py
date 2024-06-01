ecf40f1f1be416cc2b0fd22fad89f010b073611c
RetiredWizard <github@retiredwizard.com>
2024-06-01 16:37:32 -0400

    Mutli-key conversion bug fix + keys info utility

---
+++ keys.py
@@ -0,0 +1,20 @@
+def show():
+    print("     Entered Lilygo Keys    Results")
+    print("     -------------------    -------")
+    print("              _-               =")
+    print("              (+               [")
+    print("              +)               ]")
+    print("              (-               <")
+    print("              -)               >")
+    print("              _#               ^")
+    print("              _/               \\")
+    print("              -/               %")
+    print(" Speaker Key (left of enter)   $")
+    print(" Track ball up                 up arrow")
+    print(" Track ball down               down arrow")
+    print(" Track ball left               left arrow")
+    print(" Track ball right              right arrow")
+
+
+show()
+print('To re-display keys from repl type "keys.show()"')
